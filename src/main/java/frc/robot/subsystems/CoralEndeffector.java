ae1efc5cde58d6d52ad0db750973d2a74e9ae0bb
Arjun <92765324+Mindi088@users.noreply.github.com>
2025-01-23 19:02:27 -0500

    skeleton

--- src/main/java/frc/robot/subsystems/CoralEndeffector.java
+++ src/main/java/frc/robot/subsystems/CoralEndeffector.java
@@ -4,5 +4,41 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 /** Add your docs here. */
-public class CoralEndeffector {}
+public class CoralEndeffector extends SubsystemBase {
+
+    private double velocity;
+    private double position;
+    private double load;
+
+    public CoralEndeffector(){
+
+    }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+    
+    public void setPosition(double position){
+        this.position = position;
+    }
+    public double getPosition(){
+        return position;
+    }
+    public void setVelocity(double velocity){
+        this.velocity = velocity;
+    }
+    public double getVelocity(){
+            return velocity;
+    }
+    public double getLoad(){
+        return load;
+    }
+    public boolean getHasCoral(){
+    //Pull from sensor
+        return false;
+    }
+
+}

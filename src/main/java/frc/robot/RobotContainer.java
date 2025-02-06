5e864e1e89bc3f9402ec377d34e83560a7199c11
Jenni-Hu <jenni.h@frc1073.org>
2025-02-05 20:41:48 -0500

    added robotcontainer imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,47 +4,6 @@
 
 package frc.robot;
 
-<<<<<<< HEAD
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.ClimberLiftTeleop;
-import frc.robot.commands.ClimberClawTeleop;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.OI;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.ClimberClaw;
-import frc.robot.subsystems.ClimberLift;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-
-
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
-public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final OI m_oi = new OI();
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final ClimberClaw m_climberClaw = new ClimberClaw();
-  private final ClimberLift m_climberLift = new ClimberLift();
-  private final ClimberClawTeleop m_climberClawTeleop = new ClimberClawTeleop(m_climberClaw, m_oi);
-  private final ClimberLiftTeleop m_climberLiftTeleop = new ClimberLiftTeleop(m_climberLift, m_oi);
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    CommandScheduler.getInstance().setDefaultCommand(m_climberClaw, m_climberClawTeleop);
-    CommandScheduler.getInstance().setDefaultCommand(m_climberLift, m_climberLiftTeleop);
-    // Configure the trigger bindings
-=======
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -54,11 +13,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.ClimberClawTeleop;
+import frc.robot.commands.ClimberLiftTeleop;
 import frc.robot.commands.TeleopDrive;
 import frc.robot.commands.Autos.AutoCenterStart;
 import frc.robot.commands.Autos.AutoLeftStart;
 import frc.robot.commands.Autos.AutoRightStart;
+import frc.robot.commands.ZeroClaw;
 import frc.robot.subsystems.AprilTagFinder;
+import frc.robot.subsystems.ClimberClaw;
+import frc.robot.subsystems.ClimberLift;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.FieldMap;
 import frc.robot.subsystems.Localizer;
@@ -74,6 +38,10 @@ public class RobotContainer
   private final FieldMap m_fieldMap = new FieldMap();
   private final Localizer m_localizer = new Localizer(m_drivetrain, m_fieldMap, m_aprilTagFinder);
   private final MapDisplay m_MapDisplay = new MapDisplay(m_drivetrain, m_localizer, m_fieldMap);
+  private final ClimberClaw m_climberClaw = new ClimberClaw();
+  private final ClimberLift m_climberLift = new ClimberLift();
+  private final ClimberClawTeleop m_climberClawTeleop = new ClimberClawTeleop(m_climberClaw, m_OI);
+  private final ClimberLiftTeleop m_climberLiftTeleop = new ClimberLiftTeleop(m_climberLift, m_OI);
 
   private final TeleopDrive m_teleopCommand = new TeleopDrive(m_drivetrain, m_OI, m_aprilTagFinder, m_localizer);
 
@@ -97,6 +65,9 @@ public class RobotContainer
   public RobotContainer() 
   {
     CommandScheduler.getInstance().setDefaultCommand(m_drivetrain, m_teleopCommand);
+    CommandScheduler.getInstance().setDefaultCommand(m_climberClaw, m_climberClawTeleop);
+    CommandScheduler.getInstance().setDefaultCommand(m_climberLift, m_climberLiftTeleop);
+    
 
     SmartDashboard.putData(m_drivetrain);
     SmartDashboard.putData(m_OI);
@@ -119,7 +90,6 @@ public class RobotContainer
     SmartDashboard.putData("Level Chooser", m_levelChooser);
 
 
->>>>>>> main
     configureBindings();
   }
 

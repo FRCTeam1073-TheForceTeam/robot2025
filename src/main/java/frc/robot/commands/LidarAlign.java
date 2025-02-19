0e788adf839b6d9b17fa0b841210f3c4b15ce5d9
AbbyW1212 <113068753+AbbyW1212@users.noreply.github.com>
2025-02-18 20:39:49 -0500

    NEED TO TEST added finished statements to lidar align to hopefully stop when the slope is close enough to zero

--- src/main/java/frc/robot/commands/LidarAlign.java
+++ src/main/java/frc/robot/commands/LidarAlign.java
@@ -2,18 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Lidar;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class LidarAlign extends Command {
   /** Creates a new LidarAlign. */
   Lidar lidar;
+  boolean hasLine = true;
   Drivetrain drivetrain;
   double lidarSlope;
   double angleToRotate;
@@ -24,25 +27,28 @@ public class LidarAlign extends Command {
     this.lidar = lidar;
     this.drivetrain = drivetrain;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-      /*1. calculate slope of line detected by lidar */
-      double[] line = lidar.getLine();
-      lidarSlope = -line[0]/line[1];        
-      /* 2. Find arctan of the difference between their slopes - angle the robot needs to move */
-      angleToRotate = Math.atan(lidarSlope);
-      /* 3. rotate the robot that to that set angle*/
-      thetaVelocity = MathUtil.clamp(thetaController.calculate(drivetrain.getWrappedHeadingRadians(), drivetrain.getWrappedHeadingRadians() + angleToRotate), -2.0, 2.0);
-        
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.setTargetChassisSpeeds(ChassisSpeeds.fromFieldRelativeSpeeds(0, 0, thetaVelocity, new Rotation2d(drivetrain.getWrappedHeadingRadians())));
+    hasLine = (lidar.getLine() != null);
+    if(hasLine){
+      /*1. calculate slope of line detected by lidar */
+      double[] line = lidar.getLine().clone();
+      lidarSlope = -line[0]/line[1];        
+      /* 2. Find arctan of the difference between their slopes - angle the robot needs to move */
+      angleToRotate = Math.atan(lidarSlope);
+      /* 3. rotate the robot that to that set angle*/
+      thetaVelocity = MathUtil.clamp(thetaController.calculate(drivetrain.getWrappedHeadingRadians(), drivetrain.getWrappedHeadingRadians() + angleToRotate), -2.0, 2.0);
+      drivetrain.setTargetChassisSpeeds(ChassisSpeeds.fromFieldRelativeSpeeds(0, 0, thetaVelocity, new Rotation2d(drivetrain.getWrappedHeadingRadians())));
+    }
   }
 
   // Called once the command ends or is interrupted.
@@ -52,6 +58,14 @@ public class LidarAlign extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    if(!hasLine){
+      return true;
+    } 
+    if(Math.abs(lidarSlope) < 0.07){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }

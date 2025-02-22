19565e2ee33eb39382e4f03d378be52b6af00571
I v a n <isgn2031@gmail.com>
2025-02-22 17:31:44 -0500

    split up troughscorecoral
    made two new files where troughscorecoral is split up into two parts for auto
    ehehehehhe

--- src/main/java/frc/robot/commands/Autos/LeftL1.java
+++ src/main/java/frc/robot/commands/Autos/LeftL1.java
@@ -6,12 +6,14 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DrivePath;
 import frc.robot.commands.Path;
+import frc.robot.commands.TroughRaiseElevator;
+import frc.robot.commands.TroughScoreAuto;
 import frc.robot.commands.Path.Point;
 import frc.robot.commands.Path.Segment;
-import frc.robot.commands.TroughScoreCoral;
 import frc.robot.commands.ZeroElevator;
 import frc.robot.subsystems.CoralElevator;
 import frc.robot.subsystems.CoralEndeffector;
@@ -23,8 +25,8 @@ public class LeftL1
 {
     public static Command create(boolean isRed, Drivetrain drivetrain, FieldMap map, Localizer localizer, CoralEndeffector endEffector, CoralElevator elevator)  
     {
-        Pose2d tag11Pose = map.getTagRelativePose(11, 0, new Transform2d(0.45, 0, new Rotation2d(Math.PI)));
-        Pose2d tag20Pose = map.getTagRelativePose(20, 0, new Transform2d(0.45, 0, new Rotation2d(Math.PI)));
+        Pose2d tag11Pose = map.getTagRelativePose(11, 0, new Transform2d(0.5, 0, new Rotation2d(Math.PI)));
+        Pose2d tag20Pose = map.getTagRelativePose(20, 0, new Transform2d(0.5, 0, new Rotation2d(Math.PI)));
         Point start = new Point(localizer.getPose().getX(), localizer.getPose().getY());
         Point tag11 = new Point(tag11Pose.getX(), tag11Pose.getY());
         Point tag20 = new Point(tag20Pose.getX(), tag20Pose.getY());
@@ -33,21 +35,24 @@ public class LeftL1
         Path path;
         if (isRed)
         {
-            segments.add(new Segment(start, tag11, tag11Pose.getRotation().getRadians(), 1));
+            segments.add(new Segment(start, tag11, tag11Pose.getRotation().getRadians(), 1.5));
 
             path = new Path(segments, tag11Pose.getRotation().getRadians());
         }
         else
         {
-            segments.add(new Segment(start, tag20, tag20Pose.getRotation().getRadians(), 1));
+            segments.add(new Segment(start, tag20, tag20Pose.getRotation().getRadians(), 1.5));
 
             path = new Path(segments, tag20Pose.getRotation().getRadians());
         }
         
 
         return new SequentialCommandGroup(
-            new DrivePath(drivetrain, path, localizer),
-            new TroughScoreCoral(endEffector, elevator),
+            new ParallelCommandGroup(
+                new DrivePath(drivetrain, path, localizer),
+                new TroughRaiseElevator(elevator)
+            ),
+            new TroughScoreAuto(endEffector),
             new ZeroElevator(elevator)
         );
     }     
--- src/main/java/frc/robot/commands/TroughScoreAuto.java
+++ src/main/java/frc/robot/commands/TroughScoreAuto.java
@@ -4,26 +4,22 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CoralEndeffector;
-import frc.robot.subsystems.CoralElevator;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class TroughScoreCoral extends Command 
+public class TroughScoreAuto extends Command 
 {
   /** Creates a new TroughScoreCoral. */
   CoralEndeffector endeffector;
-  CoralElevator elevator;
   double velocity;
-  double targetHeight = 12.8;
+
   // height 12.8
-  public TroughScoreCoral(CoralEndeffector coralEndeffector, CoralElevator coralElevator) 
+  public TroughScoreAuto(CoralEndeffector coralEndeffector) 
   {
     endeffector = coralEndeffector;
-    elevator = coralElevator;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(endeffector, elevator);
+    addRequirements(endeffector);
   }
 
   // Called when the command is initially scheduled.
@@ -32,23 +28,14 @@ public class TroughScoreCoral extends Command
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    velocity = (targetHeight - elevator.getPosition()) * 0.6;
-    if (targetHeight > elevator.getPosition()){
-      velocity = MathUtil.clamp(velocity, 3, 12);
-    }
-    else {
-      velocity = MathUtil.clamp(velocity, -12, -3);  
-    }
-    elevator.setVelocity(velocity);
-
-    if(endeffector.getHasCoral() && finishedElevator()){
+  public void execute() 
+  {
+    if(endeffector.getHasCoral())
+    {
       endeffector.setVelocity(22);
     }
   }
-  public boolean finishedElevator() {
-    return Math.abs(elevator.getPosition() - targetHeight) < (0.01 * targetHeight);
-  }
+  
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
@@ -57,7 +44,8 @@ public class TroughScoreCoral extends Command
 
   // Returns true when the command should end.
   @Override
-  public boolean isFinished() {
+  public boolean isFinished() 
+  {
     return !endeffector.getHasCoral();
   }
 }

5336605ddf4320d4e58970a4d9346406fce1d6a6
William <92765324+Mindi088@users.noreply.github.com>
2025-03-05 19:51:43 -0500

    Smartdash board change

--- src/main/java/frc/robot/subsystems/CoralEndeffector.java
+++ src/main/java/frc/robot/subsystems/CoralEndeffector.java
@@ -110,13 +110,13 @@ public class CoralEndeffector extends SubsystemBase {
         // Send motor command:
         motor.setControl(motorVelocityVoltage.withVelocity(commandedVelocity));
 
-        SmartDashboard.putNumber("[CORAL End Effector] Coral Distance", coralDistance);
-        SmartDashboard.putBoolean("[CORAL End Effector] Has Coral", hasCoral);
-        SmartDashboard.putNumber("[CORAL End Effector] velocity", velocity);
-        SmartDashboard.putNumber("[CORAL End Effector] command", commandedVelocity);
-        SmartDashboard.putNumber("[CORAL End Effector] load", load);
-        SmartDashboard.putNumber("[CORAL End Effector] Reef Distance", reefDistance);
-        SmartDashboard.putBoolean("[CORAL End Effector] Has Reef", hasReef);
+        SmartDashboard.putNumber("Coral End Effector/Coral Distance", coralDistance);
+        SmartDashboard.putBoolean("Coral End Effector/Has Coral", hasCoral);
+        SmartDashboard.putNumber("Coral End Effector/ velocity", velocity);
+        SmartDashboard.putNumber("Coral End Effector/command", commandedVelocity);
+        SmartDashboard.putNumber("Coral End Effector/load", load);
+        SmartDashboard.putNumber("Coral End Effector/Reef Distance", reefDistance);
+        SmartDashboard.putBoolean("Coral End Effector/ Has Reef", hasReef);
     
     }
     

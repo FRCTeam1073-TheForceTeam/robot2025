5b3b848191af5d5a9ffb68ba39134b4c4d5be39d
L-i-y-a <92765324+Mindi088@users.noreply.github.com>
2025-03-22 12:09:24 -0400

    finished skeleton for algae claw

--- src/main/java/frc/robot/subsystems/AlgaeClaw.java
+++ src/main/java/frc/robot/subsystems/AlgaeClaw.java
@@ -9,33 +9,34 @@ import com.ctre.phoenix6.configs.SlotConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AlgaeClaw extends SubsystemBase {
 
   private double algaeCollectVel;
+  private double commandedAlgaeCollectVel;
   private double algaeCollectPos;
   private double algaeCollectLoad;
   private double endeffectorRotateVel;
+  private double commandedEndeffectorRotateVel;
   private double endeffectorRotatePos;
   private double endeffectorRotateLoad;
 
   private boolean hasAlgae;
-  private boolean algaeCollectBrakeMode;
-  private boolean endeffectorRotateBrakeMode;
+  private boolean algaeCollectBrakeMode = true;
+  private boolean endeffectorRotateBrakeMode = true;
 
   private TalonFX algaeCollectMotor;
   private TalonFX endeffectorRotateMotor;
-  private CANcoder encoder;
   private VelocityVoltage algaeCollectMotorVelocityVoltage;
   private PositionVoltage algaeCollectMotorPositionVoltage;
-  private VelocityVoltage endeffectorRotatorMotorVelocityVoltage;
-  private PositionVoltage endeffectorRotatorMotorPositionVoltage;
+  private VelocityVoltage endeffectorRotateMotorVelocityVoltage;
+  private PositionVoltage endeffectorRotateMotorPositionVoltage;
 
   private final double algaeCollectMinPos = 0; //TODO set min and max positions
   private final double algaeCollectMaxPos = 0;
@@ -52,19 +53,58 @@ public class AlgaeClaw extends SubsystemBase {
   private final double endeffectorRotateMotorKV = 0.12;
 
   /** Creates a new AlgaeClaw. */
-  public AlgaeClaw() {}
+  public AlgaeClaw() {
+    algaeCollectMotor = new TalonFX(0);//TODO find device IDs
+    endeffectorRotateMotor = new TalonFX(0);
+
+    algaeCollectMotorVelocityVoltage = new VelocityVoltage(0).withSlot(0);
+    algaeCollectMotorPositionVoltage = new PositionVoltage(0).withSlot(0);
+    endeffectorRotateMotorVelocityVoltage = new VelocityVoltage(0).withSlot(0);
+    endeffectorRotateMotorPositionVoltage = new PositionVoltage(0).withSlot(0);
+
+    configureHardware();
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    algaeCollectVel = algaeCollectMotor.getVelocity().getValueAsDouble(); 
+    algaeCollectPos = algaeCollectMotor.getPosition().getValueAsDouble();
+    algaeCollectLoad = algaeCollectMotor.getTorqueCurrent().getValueAsDouble();
+    endeffectorRotateVel = endeffectorRotateMotor.getVelocity().getValueAsDouble(); 
+    endeffectorRotatePos = endeffectorRotateMotor.getPosition().getValueAsDouble();
+    endeffectorRotateLoad = endeffectorRotateMotor.getTorqueCurrent().getValueAsDouble();
+
+    if (algaeCollectPos >= algaeCollectMaxPos){
+      commandedAlgaeCollectVel = Math.min(commandedAlgaeCollectVel, 0);
+    }
+    if (algaeCollectPos <= algaeCollectMinPos){
+      commandedAlgaeCollectVel = Math.max(commandedAlgaeCollectVel, 0);
+    }
+    if (endeffectorRotatePos >= algaeCollectMaxPos){
+      commandedEndeffectorRotateVel = Math.min(commandedEndeffectorRotateVel, 0);
+    }
+    if (endeffectorRotatePos <= algaeCollectMinPos){
+      commandedEndeffectorRotateVel = Math.max(commandedEndeffectorRotateVel, 0);
+    }
+
+    algaeCollectMotor.setControl(algaeCollectMotorVelocityVoltage.withVelocity(commandedAlgaeCollectVel));
+    endeffectorRotateMotor.setControl(endeffectorRotateMotorVelocityVoltage.withVelocity(commandedEndeffectorRotateVel));
+
+    SmartDashboard.putNumber("[AlgaeClaw] algae collect velocity", algaeCollectVel);
+    SmartDashboard.putNumber("[AlgaeClaw] algae collect position", algaeCollectPos);
+    SmartDashboard.putNumber("[AlgaeClaw] algae collect commanded velocity", commandedAlgaeCollectVel);
+    SmartDashboard.putNumber("[AlgaeClaw] endeffector rotate velocity", endeffectorRotateVel);
+    SmartDashboard.putNumber("[AlgaeClaw] endeffector rotate position", endeffectorRotatePos);
+    SmartDashboard.putNumber("[AlgaeClaw] endeffector rotate commanded velocity", commandedEndeffectorRotateVel);
+    SmartDashboard.putBoolean("[AlgaeClaw] has algae", hasAlgae);
   }
 
   public void setAlgaeCollectorVel(double newVel){
-    algaeCollectVel = newVel;
+    commandedAlgaeCollectVel = newVel;
   }
 
   public void setEndffectorRotatorVel(double newVel){
-    endeffectorRotateVel = newVel;
+    commandedEndeffectorRotateVel = newVel;
   }
 
   public void setAlgaeCollectorMotorBrakeMode(boolean mode){
@@ -159,8 +199,8 @@ public class AlgaeClaw extends SubsystemBase {
 
     CurrentLimitsConfigs motorCurrentLimitsConfigs = new CurrentLimitsConfigs(); //TODO check if these next 4 lines need to be copied
     motorCurrentLimitsConfigs.withSupplyCurrentLimitEnable(true)
-                            .withSupplyCurrentLimit(15)
-                            .withSupplyCurrentLowerTime(0.25);
+                             .withSupplyCurrentLimit(15)
+                             .withSupplyCurrentLowerTime(0.25);
 
     algaeCollectMotor.getConfigurator().apply(motorCurrentLimitsConfigs);
     endeffectorRotateMotor.getConfigurator().apply(motorCurrentLimitsConfigs);

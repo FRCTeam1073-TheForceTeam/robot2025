dc7d6214e44edfce7798f0a355378e55ade562f0
William Van Uitert <82126044+SnowballGus@users.noreply.github.com>
2025-03-23 00:16:51 -0400

    added sound when button 7

--- sound.py
+++ sound.py
@@ -1,31 +1,45 @@
-import winsound
+# import winsound
 import pygame
 
 running = True
 
 # winsound.PlaySound("C:/Users/FRC1073/Desktop/Mario-coin-sound.wav", winsound.SND_FILENAME)
+# pygame docs: https://www.pygame.org/docs/ref/joystick.html
+# work for bluetooth connected controller but not hard connect
 
 pygame.init()
 pygame.joystick.init()
+pygame.mixer.init()
+sound = pygame.mixer.Sound("/Users/williamvanuitert/Desktop/Mario-coin-sound.mp3")
+
 
 joystick_count = pygame.joystick.get_count()
+
 if joystick_count == 0:
 	print("No joysticks found.")
-	pygame.quit()
+	pygame.QUIT
 else:
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
     print(f"Joystick {joystick.get_name()} initialized")
+
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-		elif event.type == pygame.JOYBUTTONDOWN:
+		if event.type == pygame.JOYBUTTONDOWN:
 			print("Button pressed:", event.button)
-		elif event.type == pygame.JOYBUTTONUP:
+
+			if(event.button == 7):
+				sound.play()
+
+		if event.type == pygame.JOYBUTTONUP:
 			print("Button released:", event.button)
-		elif event.type == pygame.JOYAXISMOTION:
+
+		if event.type == pygame.JOYAXISMOTION and abs(event.value) > 0.5:
 			print("Axis moved:", event.axis, "Value:", event.value)
-		elif event.type == pygame.JOYHATMOTION:
+
+		if event.type == pygame.JOYHATMOTION:
 			print("Hat switch moved:", event.hat, "Value:", event.value)
-pygame.quit()
+
+pygame.QUIT
